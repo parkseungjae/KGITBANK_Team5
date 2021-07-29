5ee95b7a17f4695809393ae2da0a2b3cafc1d07a
2ggg <larunaru0386@gmail.com>
2021-07-29 15:15:15 +0900

    4번 문제

--- Juhee0729/Q04_juhee.java
+++ Juhee0729/Q04_juhee.java
@@ -1,11 +1,20 @@
 package Juhee0729;
 
+import java.util.Scanner;
+
 public class Q04_juhee {
 /*
 # 4번. 박승재 문제
 1부터 100까지 숫자중 입력받은 수의 배수들만 출력받게하라
  */
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("수 입력>>");
+		int num = scanner.nextInt();
+		for(int i=1; i<=100; i++) {
+			if(i%num==0) {
+				System.out.println(i);
+			}
+		}
 	}
 }
