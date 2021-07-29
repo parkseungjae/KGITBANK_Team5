3c694e01dc7cfd8a91f2f1f7791a4c490dcf8e43
2ggg <larunaru0386@gmail.com>
2021-07-29 15:07:16 +0900

    3번 문제

--- Juhee0729/Q03_juhee.java
+++ Juhee0729/Q03_juhee.java
@@ -6,6 +6,10 @@ public class Q03_juhee {
 for문을 활용해서 15부터 65까지 홀수만 출력하세요
  */
 	public static void main(String[] args) {
-		
+		for(int i=15; i<=65; i++) {
+			if(i%2==1) {
+				System.out.println(i);
+			}
+		}
 	}
 }
--- Juhee0729/Q04_juhee.java
+++ Juhee0729/Q04_juhee.java
@@ -5,5 +5,7 @@ public class Q04_juhee {
 # 4번. 박승재 문제
 1부터 100까지 숫자중 입력받은 수의 배수들만 출력받게하라
  */
-
+	public static void main(String[] args) {
+		
+	}
 }
