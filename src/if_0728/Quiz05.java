270da3eec4f69721c5086bfe0598a66b73a7e55d
wonjoun <gmrepf2273@naver.com>
2021-07-29 16:00:38 +0900

    if_0728_quiz5

--- src/if_0728/Quiz05.java
+++ src/if_0728/Quiz05.java
@@ -1,5 +1,73 @@
 package if_0728;
 
+import java.util.Scanner;
+
 public class Quiz05 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int num1;
+		int num2;
+		int num3;
+
+		System.out.println("수 입력 : ");
+		num1 = input.nextInt();
+		num2 = input.nextInt();
+		num3 = input.nextInt();
+		if(num1 >= num2 && num1 >= num3)
+		{
+			System.out.println("최대값 : "+ num1);
+		}else if(num2 >= num1 && num2 >= num3) {
+			System.out.println("최대값 : " + num2);
+		}	else if (num3 >= num1 && num3 >= num2) {
+			System.out.println("최대값 : " + num3);
+		}	
+
+		if(num1 <= num2 && num1 <= num3)
+		{
+			System.out.println("최소값 : "+ num1);
+		}else if(num2 <= num1 && num2 <= num3) {
+			System.out.println("최소값 : " + num2);
+		}	else if(num3 <= num1 && num3 <= num2) {
+			System.out.println("최소값 : " + num3);
+		}	
+
+
+
+
+		if(num1 >= num2 && num1 >= num3 && num1%3==0)
+			System.out.println("최대값은 3의 배수입니다");
+		else if(num2 >= num1 && num2 >= num3 && num2%3==0)
+			System.out.println("최대값은 3의 배수입니다");
+		else if(num3 >= num2 && num3 >= num1 && num3%3==0)
+			System.out.println("최대값은 3의 배수입니다");
+
+		if(num1 <= num2 && num1 <= num3 && num1%3==0)
+			System.out.println("최소값은 3의 배수입니다");
+		else if(num2 <= num1 && num2 <= num3 && num2%3==0)
+			System.out.println("최소값은 3의 배수입니다");
+		else if(num3 <= num2 && num3 <= num1 && num3%3==0)
+			System.out.println("최소값은 3의 배수입니다");
+
+		if(num1 >= num2 && num1 >= num3 && num1%3!=0)
+			System.out.println("최대값은 3의 배수가 아닙니다");
+		else if(num2 >= num1 && num2 >= num3 && num2%3!=0)
+			System.out.println("최대값은 3의 배수가 아닙니다");
+		else if(num3 >= num2 && num3 >= num1 && num3%3!=0)
+			System.out.println("최대값은 3의 배수가 아닙니다");
+
+		if(num1 <= num2 && num1 <= num3 && num1%3!=0)
+			System.out.println("최소값은 3의 배수가 아닙니다");
+		else if(num2 <= num1 && num2 <= num3 && num2%3!=0)
+			System.out.println("최소값은 3의 배수가 아닙니다");
+		else if(num3 <= num2 && num3 <= num1 && num3%3!=0)
+			System.out.println("최대값은 3의 배수가아닙니다");
+
+
+
+
+
+
+	}
 
 }
