5515480dce5ea5ab76e34e31ca438249dc5f54ff
parkseungjae <tmd0915mp@naver.com>
2021-07-30 09:13:03 +0900

    성택님 문제 대용 주희님문제 추가

--- src/quiz_210729_for/TeamQuiz04_for.java
+++ src/quiz_210729_for/TeamQuiz04_for.java
@@ -1,5 +1,14 @@
 package quiz_210729_for;
 
 public class TeamQuiz04_for {
+	//이주희 문제 보너스.
+	public static void main(String[] args) {
+		for(int i=1; i<=4; i++ ) {
+			for(int j = 1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
 
+		}
+	}
 }
